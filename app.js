ff7d3270e432419f25984c2f883d71753b64f18e
Benja Aguilar <benjaaguilar02@gmail.com>
2024-12-11 12:00:57 -0300

    chore: add passport configuration

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'node:path';
+import passport from 'passport';
+import configurePassport from './config/passportConfig.js';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/postRoutes.js';
@@ -8,9 +10,14 @@ import commentRoutes from './routes/commentRoutes.js';
 
 const app = express();
 
+dotenv.config();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), 'public')));
+
+configurePassport(passport)
+app.use(passport.initialize());
 
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);

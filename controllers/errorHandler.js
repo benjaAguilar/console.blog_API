f6e48c6ee6182b989196972e1efb67021da0f81a
Benja Aguilar <benjaaguilar02@gmail.com>
2024-12-11 17:02:11 -0300

    feat: add error handle middleware
    this manages errors and returns correct
    status codes and meaningfull messages

---
+++ controllers/errorHandler.js
@@ -0,0 +1,43 @@
+import Errors from '../lib/customError.js';
+
+async function errorHandler(err, req, res, next) {
+    
+    console.log(err);
+    console.log('----------');
+    console.log(err.message);
+
+    if(err instanceof Errors.validationError){
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            validationErrors: err.validationErrors
+        })
+
+        return;
+    }
+
+    if(err instanceof Errors.customError){
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        });
+
+        return;
+    }
+
+
+    if(err instanceof Errors.dbError){
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            dbErr: err.dbErr
+        })
+
+        return;
+    }
+
+
+    res.status(500).json({success: false, message: 'Internal server Error'});
+}
+
+export default errorHandler;

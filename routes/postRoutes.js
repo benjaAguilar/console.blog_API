477417d0f8a40c755304624ff3cbb32375b9b552
Benja Aguilar <benjaaguilar02@gmail.com>
2024-12-12 12:20:45 -0300

    feat: add POST /posts route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import postController from '../controllers/postController.js';
+import tryCatch from '../lib/tryCatch.js';
+import upload from '../config/multer.js';
+import passport from "passport";
 
 const router = Router();
 
 router.get('/', postController.getPosts);
+
+router.post(
+    '/', 
+    passport.authenticate('jwt', {session: false}), 
+    upload.single('post'), 
+    tryCatch(postController.createPost)
+);
 
 export default router;
 

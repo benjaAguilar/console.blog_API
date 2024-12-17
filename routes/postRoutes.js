94bf62cd40115e8af279deec91f8b282d59228f7
Benja Aguilar <benjaaguilar02@gmail.com>
2024-12-17 12:28:13 -0300

    feat: add get, post, delete comment routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,7 @@ import postController from '../controllers/postController.js';
 import tryCatch from '../lib/tryCatch.js';
 import upload from '../config/multer.js';
 import passport from "passport";
+import commentController from "../controllers/commentController.js";
 
 const router = Router();
 
@@ -28,6 +29,24 @@ router.put(
     passport.authenticate('jwt', {session: false}),
     upload.single('post'),
     tryCatch(postController.updatePost)
+);
+
+//COMMENTS
+router.get(
+    '/:postId/comments',
+    tryCatch(commentController.getComments)
+);
+
+router.post(
+     '/:postId/comments',
+     passport.authenticate('jwt', {session: false}),
+     tryCatch(commentController.createComment)
+);
+
+router.delete(
+    '/comments/:commentId',
+    passport.authenticate('jwt', {session: false}),
+    tryCatch(commentController.deleteComment)
 );
 
 export default router;

2275bd7415112f9b3079b4dd48966188bb1fe8fa
Benja Aguilar <benjaaguilar02@gmail.com>
2024-12-18 19:54:02 -0300

    ref: createComment and updateComment middleware

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,7 @@
 import commentQueries from "../db/commentQueries.js";
+import validator from "../config/validator.js";
 import Errors from "../lib/customError.js";
+import tryCatch from "../lib/tryCatch.js";
 
 async function getComments(req, res, next){
     const postId = parseInt(req.params.postId);
@@ -11,19 +13,31 @@ async function getComments(req, res, next){
     });
 }
 
-async function createComment(req, res, next){
-    const user = req.user;
-    const postId = parseInt(req.params.postId);
+const createComment = [
+    validator.validateComment,
+    tryCatch(
+        async (req, res, next) => {
+            const validationErrors = validationResult(req);
+            if(!validationErrors.isEmpty()){
+                return next(
+                    new Errors.validationError('Invalid field', 400, validationErrors.array())
+                );
+            }
 
-    const { content } = req.body;
-    const comment = await commentQueries.createComment(content, user.id, postId);
-
-    res.json({
-        success: true,
-        message: 'Commented!',
-        comment
-    });
-}
+            const user = req.user;
+            const postId = parseInt(req.params.postId);
+        
+            const { content } = req.body;
+            const comment = await commentQueries.createComment(content, user.id, postId);
+        
+            res.json({
+                success: true,
+                message: 'Commented!',
+                comment
+            });
+        }
+    )
+]
 
 async function deleteComment(req, res, next){
     const user = req.user;
@@ -48,29 +62,41 @@ async function deleteComment(req, res, next){
     });
 }
 
-async function updateComment(req, res, next){
-    const user = req.user;
-    const commentId = parseInt(req.params.commentId);
+const updateComment = [
+    validator.validateComment,
+    tryCatch(
+        async (req, res, next) => {
+            const validationErrors = validationResult(req);
+            if(!validationErrors.isEmpty()){
+                return next(
+                    new Errors.validationError('Invalid field', 400, validationErrors.array())
+                );
+            }
 
-    const comment = await commentQueries.getCommentById(commentId);
-
-    if(!comment){
-        return next(new Errors.customError('Comment not found', 404));
-    }
-
-    if(comment.ownerId !== user.id){
-        return next(new Errors.customError('You dont have permissions to edit this comment', 401));
-    }
-
-    const { content } = req.body;
-    const updatedComment = await commentQueries.updateComment(commentId, content);
-
-    res.json({
-        success: true,
-        message: 'Comment Updated!',
-        updatedComment
-    });
-}
+            const user = req.user;
+            const commentId = parseInt(req.params.commentId);
+        
+            const comment = await commentQueries.getCommentById(commentId);
+        
+            if(!comment){
+                return next(new Errors.customError('Comment not found', 404));
+            }
+        
+            if(comment.ownerId !== user.id){
+                return next(new Errors.customError('You dont have permissions to edit this comment', 401));
+            }
+        
+            const { content } = req.body;
+            const updatedComment = await commentQueries.updateComment(commentId, content);
+        
+            res.json({
+                success: true,
+                message: 'Comment Updated!',
+                updatedComment
+            });
+        }
+    )
+]
 
 const commentController = {
     getComments,

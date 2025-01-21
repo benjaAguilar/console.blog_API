232dfe0c71ff913d4dc8e2a61fc772530584e2fd
Benja Aguilar <benjaaguilar02@gmail.com>
2025-01-21 11:51:24 -0300

    feat: add /authUser route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
+import passport from "passport";
+import tryCatch from "../lib/tryCatch.js";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
 
 router.post("/login", userController.loginUser);
+
+router.get(
+  "/authUser",
+  passport.authenticate("jwt", { session: false }),
+  tryCatch(userController.getAuthUser),
+);
 
 export default router;
